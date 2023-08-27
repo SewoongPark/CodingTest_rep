19d2603a33a76ffae017fa94c7dd3956f1ade092
박세웅 <98893325+SewoongPark@users.noreply.github.com>
2023-08-28 00:37:18 +0900

    Update 행렬의 덧셈.py
    코드 수정

--- 프로그래머스/lv1/12950. 행렬의 덧셈/행렬의 덧셈.py
+++ 프로그래머스/lv1/12950. 행렬의 덧셈/행렬의 덧셈.py
@@ -1,11 +1,9 @@
 def solution(arr1, arr2):
     answer = []
-    cnt = 0 #행의 길이(한 개의 리스트에 담긴 원소의 수)와 같을 때 리스트로 한번 더 감싸기
-    changer = 0
-    row = len(arr1)
+    
     for a1, a2 in zip(arr1, arr2):
         totalList = []
-        for idx, (num1, num2) in enumerate(zip(a1, a2)):
+        for num1, num2 in zip(a1, a2):
             totalList.append(num1 + num2)
         answer.append(totalList)
     return answer
