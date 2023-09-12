399d3899f44806314784e3ae0e3b1c63d866c445
박세웅 <98893325+SewoongPark@users.noreply.github.com>
2023-09-13 01:04:32 +0900

    Update 별 찍기 － 7.py

--- 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.py
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.py
@@ -1,9 +1,9 @@
 n = int(input())
-total_len = 2 * (int(n) - 1)
 
+# 윗 부분 출력
 for i in range(n):
-    print(" " * (n - 1 - i) + "*" * (2 * i + 1))
-#     if i > 8:
-        
-for i in range(n-1):
-    print(" "*(i + 1) + "*" * (-2 * i + (2*n -3)))
+    print(" " * (n - i - 1) + "*" * ((i * 2) + 1))
+
+# 아래 부분 출력
+for i in range(n - 2, -1, -1): 
+    print(" " * (n - i - 1) + "*" * ((i * 2) + 1))
